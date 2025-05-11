daa475e516ee24de4505facce4a0339b5b66f5a9
madhuroshan <madhuroshan2102@gmail.com>
2025-05-11 11:33:10 +0530

    inventory page

--- app/components/ClothingItemCard.tsx
+++ app/components/ClothingItemCard.tsx
@@ -1,68 +1,72 @@
-import React, { useState } from 'react'
-import { FaEdit } from 'react-icons/fa'
+import React, { useState } from "react";
+import { FaEdit } from "react-icons/fa";
 
 type Props = {
-	item: {
-		id: number
-		image_file_name: string
-	}
-	category: string
-}
+  item: {
+    id: number;
+    image_file_name: string;
+  };
+  category: string;
+};
 
 type ClothingItemModalProps = {
-	isEditing: boolean
-	setIsEditing: (isEditing: boolean) => void
-}
+  setIsEditing: (isEditing: boolean) => void;
+};
 
 const ClothingItemCard = (props: Props) => {
-	const { item, category } = props
-	const [isEditing, setIsEditing] = useState(false)
+  const { item, category } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [showEditButton, setShowEditButton] = useState(false);
 
-	function handleEdit() {
-		setIsEditing(true)
-	}
+  function handleEdit() {
+    setIsEditing(true);
+  }
 
-	return (
-		<div key={item.id} className='inventory-item'>
-			<div className='inventory-item-icons top-right'>
-				<FaEdit
-					className='inventory-item-icon'
-					onClick={() => {
-						handleEdit()
-					}}
-				/>
-			</div>
+  return (
+    <div
+      key={item.id}
+      className="flex flex-col items-center justify-center hover:translate-y-[-4px] hover:border-2 hover:border-black hover:shadow-lg w-[200px] transition-all duration-300 ease-in-out relative rounded-md bg-secondary "
+      onMouseOver={() => setShowEditButton(true)}
+      onMouseLeave={() => setShowEditButton(false)}
+    >
+      <div
+        className={`absolute top-2 right-2 flex gap-2 ${
+          showEditButton ? "" : "hidden"
+        }`}
+      >
+        <FaEdit
+          className="inventory-item-icon"
+          onClick={() => {
+            handleEdit();
+          }}
+        />
+      </div>
 
-			<div className='inventory-image-wrapper'>
-				<img
-					src={`/assets/inverted-triangle/${item.image_file_name}`}
-					alt={item.image_file_name}
-					className='inventory-image'
-				/>
-			</div>
-			<p className='inventory-item-name'>{item.id}</p>
+      <div className="inventory-image-wrapper">
+        <img
+          src={`/assets/inverted-triangle/${item.image_file_name}`}
+          alt={item.image_file_name}
+          width={160}
+          height={160}
+          className="inventory-image"
+        />
+      </div>
+      <p className="inventory-item-name">{item.id}</p>
+      {isEditing && <ClothingItemModal setIsEditing={setIsEditing} />}
+    </div>
+  );
+};
 
-			<ClothingItemModal
-				isEditing={isEditing}
-				setIsEditing={setIsEditing}
-			/>
-		</div>
-	)
+function ClothingItemModal({ setIsEditing }: ClothingItemModalProps) {
+  return (
+    <div className="w-full bg-gray-500 h-full top-0 left-0 fixed z-50">
+      <div
+        className={`max-w-md max-h-72 bg-primary top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] `}
+      >
+        ClothingItemModal
+      </div>
+    </div>
+  );
 }
 
-function ClothingItemModal({
-	isEditing,
-	setIsEditing,
-}: ClothingItemModalProps) {
-	return (
-		<div
-			className={`max-w-md max-h-72 bg-primary fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ${
-				isEditing ? `flex` : `hidden`
-			} z-50`}
-		>
-			ClothingItemModal
-		</div>
-	)
-}
-
-export default ClothingItemCard
+export default ClothingItemCard;
