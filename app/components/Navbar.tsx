388f67eaaa054c50d46fc1bcad279f0c639c9d4f
madhuroshan <madhuroshan2102@gmail.com>
2025-05-23 11:58:19 +0530

    inventory and menubar ui

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,26 @@
 "use client";
 import { SignedIn } from "@clerk/nextjs";
-import LogoutButton from "./LogoutButton";
 import Logo from "./Logo";
 import ProfileIcon from "./ProfileIcon";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 function Navbar() {
+  const [title, setTitle] = useState("");
+  const pathname = usePathname();
+  useEffect(() => {
+    const pathnameParts = pathname.split("/");
+    if (pathname.includes("inventory")) {
+      setTitle("Fit Guide");
+    }
+  }, [pathname]);
+
   return (
-    <nav className="inventory-navigation-bar">
+    <nav className="inventory-navigation-bar mt-2">
       <div className="inventory-navigation-content">
         <Logo />
+
+        <p className="text-xl font-semibold text-center">{title}</p>
         <div className="cursor-pointer">
           <SignedIn>
             <div className="flex gap-4 text-xs items-center justify-center">
