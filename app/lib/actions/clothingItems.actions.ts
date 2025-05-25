77556b50cfed1d3b74abcd8cded031432c8149e9
madhuroshan <madhuroshan2102@gmail.com>
2025-05-25 16:54:22 +0530

    clothing action query change

--- app/lib/actions/clothingItems.actions.ts
+++ app/lib/actions/clothingItems.actions.ts
@@ -1,42 +1,42 @@
-'use server'
+"use server";
 
-import { DEFAULT_WARDROBE } from '@/app/constants/utils'
-import pool from '../database/db'
-import { getUserId } from '../database/getUserId'
+import { DEFAULT_WARDROBE } from "@/app/constants/utils";
+import pool from "../database/db";
+import { getUserId } from "../database/getUserId";
 
 export const createUserWardrobe = async (userId: number, bodyType?: string) => {
-	const client = await pool.connect()
+  const client = await pool.connect();
 
-	try {
-		await client.query('SET search_path TO capsulify_live')
-		const defaultItems = DEFAULT_WARDROBE.INVERTED_TRIANGLE
-		const createWardrobeQuery = `
-    INSERT INTO user_clothing_variants (user_id, clothing_variant_id)
-    SELECT $1, $2`
-		for (const item of defaultItems) {
-			await client.query(createWardrobeQuery, [userId, item])
-		}
-		console.log('User wardrobe created successfully')
-	} catch (error) {
-		console.error('Error creating user wardrobe:', error)
-		throw new Error('Failed to create user wardrobe')
-	} finally {
-		client.release()
-	}
-}
+  try {
+    await client.query("SET search_path TO capsulify_live");
+    const defaultItems = DEFAULT_WARDROBE.INVERTED_TRIANGLE;
+    const createWardrobeQuery = `
+  INSERT INTO user_clothing_variants (user_id, clothing_variant_id)
+  SELECT $1, UNNEST($2::int[])
+`;
+
+    await client.query(createWardrobeQuery, [userId, defaultItems]);
+    console.log("User wardrobe created successfully");
+  } catch (error) {
+    console.error("Error creating user wardrobe:", error);
+    throw new Error("Failed to create user wardrobe");
+  } finally {
+    client.release();
+  }
+};
 
 export const getUserWardrobe = async (clerkId: string) => {
-	const userId = await getUserId()
-	if (!userId) {
-		throw new Error('User not found')
-	}
-	
-	const client = await pool.connect()
+  const userId = await getUserId();
+  if (!userId) {
+    throw new Error("User not found");
+  }
 
-	try {
-		await client.query('SET search_path TO capsulify_live')
+  const client = await pool.connect();
 
-		const getWardrobeQuery = `
+  try {
+    await client.query("SET search_path TO capsulify_live");
+
+    const getWardrobeQuery = `
         SELECT 
         ucv.id AS id,
         ci.category_id,
@@ -46,48 +46,48 @@ export const getUserWardrobe = async (clerkId: string) => {
         JOIN clothing_variants cv ON ucv.clothing_variant_id = cv.id
         JOIN clothing_items ci ON cv.clothing_item_id = ci.id
         WHERE ucv.user_id = $1
-        `
+        `;
 
-		const wardrobe = await client.query(getWardrobeQuery, [userId])
-		const clothingVariantIds = wardrobe.rows.map(
-			(item) => item.clothing_variant_id
-		)
+    const wardrobe = await client.query(getWardrobeQuery, [userId]);
+    const clothingVariantIds = wardrobe.rows.map(
+      (item) => item.clothing_variant_id
+    );
 
-		// SEGREGATE CLOTHING VARIANTS BY CATEGORY
-		const segregatedWardrobe = wardrobe.rows.reduce((acc, item) => {
-			const categoryId = item.category_id
-			if (!acc[categoryId]) {
-				acc[categoryId] = []
-			}
-			acc[categoryId].push(item)
-			return acc
-		}, {})
-		console.log('User wardrobe retrieved successfully:', segregatedWardrobe)
+    // SEGREGATE CLOTHING VARIANTS BY CATEGORY
+    const segregatedWardrobe = wardrobe.rows.reduce((acc, item) => {
+      const categoryId = item.category_id;
+      if (!acc[categoryId]) {
+        acc[categoryId] = [];
+      }
+      acc[categoryId].push(item);
+      return acc;
+    }, {});
+    console.log("User wardrobe retrieved successfully:", segregatedWardrobe);
 
-		return segregatedWardrobe
-	} catch (error) {
-		console.error('Error getting user wardrobe:', error)
-		throw new Error('Failed to get user wardrobe')
-	} finally {
-		client.release()
-	}
-}
+    return segregatedWardrobe;
+  } catch (error) {
+    console.error("Error getting user wardrobe:", error);
+    throw new Error("Failed to get user wardrobe");
+  } finally {
+    client.release();
+  }
+};
 
 // TODO: Move this to middleware to make it available to every HTTP request.
 async function getUserIdByClerkId(clerkId: string) {
-	const client = await pool.connect()
+  const client = await pool.connect();
 
-	try {
-		await client.query('SET search_path TO capsulify_live')
-		const getUserIdQuery = `
+  try {
+    await client.query("SET search_path TO capsulify_live");
+    const getUserIdQuery = `
 			SELECT id FROM users WHERE clerk_id = $1
-		`
-		const userId = await client.query(getUserIdQuery, [clerkId])
-		return Number(userId.rows[0].id)
-	} catch (error) {
-		console.error('Error getting user ID by clerk ID:', error)
-		throw new Error('Failed to get user ID by clerk ID')
-	} finally {
-		client.release()
-	}
+		`;
+    const userId = await client.query(getUserIdQuery, [clerkId]);
+    return Number(userId.rows[0].id);
+  } catch (error) {
+    console.error("Error getting user ID by clerk ID:", error);
+    throw new Error("Failed to get user ID by clerk ID");
+  } finally {
+    client.release();
+  }
 }
